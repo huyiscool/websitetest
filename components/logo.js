1bbf6f3ba64c7cfcfc45317716dd5e9aa53de66d
Huy Truong <huy.truong.cal@gmail.com>
2021-12-08 19:01:48 -0800

    added logo

--- components/logo.js
+++ components/logo.js
@@ -20,20 +20,20 @@ const LogoBox = styled.span`
 `
 
 const Logo = () => {
-  const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+  const footPrintImg = `/images/cat${useColorModeValue('', '-dark')}.png`
 
   return (
     <Link href="/">
       <a>
         <LogoBox>
-          <Image src={footPrintImg} width={20} height={20} alt="logo" />
+          <Image src={footPrintImg} width={25} height={25} alt="logo" />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
             fontWeight="bold"
             ml={3}
           >
-            Tester
+            CAT 125
           </Text>
         </LogoBox>
       </a>
